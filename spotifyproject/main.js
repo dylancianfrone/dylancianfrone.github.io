ec163c81d0068e6808c34d4a4edb48e901d2720b
Dylan Cianfrone <dylancianfrone@gmail.com>
2020-07-25 13:02:34 -0400

    starting sp

---
+++ spotifyproject/main.js
@@ -0,0 +1,10 @@
+
+
+// var artist = prompt("Enter an artist name");
+// document.getElementById("artist").innerHTML = artist;
+
+function getArtist(){
+  artist = document.getElementById("getartist").value;
+  document.getElementById("sayartist").innerHTML = artist;
+
+}
